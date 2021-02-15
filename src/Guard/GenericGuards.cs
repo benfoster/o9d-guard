a5ef2aa511a4f79ebfea5d5f521404b7debe58b0
Ben Foster <ben@planetcloud.co.uk>
2021-02-15 12:24:05 +0000

    Removes dodgy code

--- src/Guard/GenericGuards.cs
+++ src/Guard/GenericGuards.cs
@@ -26,9 +26,7 @@ namespace O9d.Guard
         /// </example>
         [DebuggerStepThrough]
         public static T NotNull<T>(this T value, [NotNull] string name)
-        {
-            string sql = "SELECT * FROM Customers where Name =" + name;
-            
+        {           
             return value ?? throw new ArgumentNullException(name);
         }
     }
