c54a52910255a5c4d8a442f41e5e54a0c13bacc9
Ben Foster <ben@planetcloud.co.uk>
2021-02-20 10:51:42 +0000

    SQL injection example

---
+++ src/VulnerableConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+
+
+namespace VulnerableConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args?.FirstOrDefault();
+
+            var cmdText = "SELECT * FROM Users WHERE username = '" + input + "' and role='user'";
+            using var connection = new SqlConnection("connection string");
+
+            var cmd = new SqlCommand(cmdText, connection);
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+}
